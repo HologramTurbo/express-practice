eb77cf32bb3b39d5d8f3e25a286fb91816d3b4dc
Jaime <jaimesof82@gmail.com>
2019-10-15 20:28:52 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger')
+const app = express();
+
+// INIT MIDDLEWARE
+// app.use(logger);
+
+// SET A STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
+
+// MEMBERS API ROUTES
+app.use('/api/members', require('./routes/api/members'))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ members.js
@@ -0,0 +1,20 @@
+let members = [{
+        id: 1,
+        name: 'Wonder Woman',
+        email: 'bigbooty@email.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Green Lantern',
+        email: 'player@email.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: ' Uzumaki Naruto',
+        email: 'gotime@email.com',
+        status: 'active'
+    }
+];
+module.exports = members;
